190d0f6ac8a7874081825bb221e5638b45c9a94a
Yashraj Jadhav <yashrajjadhav49@gmail.com>
2023-02-01 17:29:43 +0530

    Add files via upload

---
+++ BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Yashraj jadhav on 06/01/23.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
